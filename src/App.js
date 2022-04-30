81105a91245a7d865d246df87f5028e5ae8f3141
Andres Montoya Angulo <jmontoyaan@unsa.edu.pe>
2022-04-30 11:17:13 -0500

    add cv

--- src/App.js
+++ src/App.js
@@ -30,27 +30,41 @@ function App() {
             Desarrollado por <b>Andres Montoya</b>
         </p>
         <div className="row">
-          <a className="pagina" href="https://www.linkedin.com/in/amontoya224/" target="_blank">
-            <img className='pagina-logo' src={linkedin} alt='logo linkedin'/>
-          </a>
-          <a className="pagina" href="https://github.com/AMontoya224" target="_blank">
-            <img className='pagina-logo' src={github} alt='logo github'/>
-          </a>
-          <a className="pagina" href="https://scholar.google.com/citations?user=LI023coAAAAJ&hl=es" target="_blank">
-            <img className='pagina-logo' src={scholar} alt='logo scholar'/>
-          </a>
-          <a className="pagina" href='https://drive.google.com/file/d/1VMg9fMJClXj3eSKCVz18-bn8OM4CR-tn/view?usp=sharing' target="_blank">
-            <img className='pagina-logo' src={cv} alt='logo cv'/>
-          </a>
-          <a className="pagina" href="https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=140140" target="_blank">
-            <img className='pagina-logo' src={concytec} alt='logo concytec'/>
-          </a>
-          <a className="pagina" href="https://web.facebook.com/juan.andres.montoya.angulo" target="_blank">
-            <img className='pagina-logo' src={facebook} alt='logo facebook'/>
-          </a>
-          <a className="pagina" href="https://www.youtube.com/channel/UC94EeNbFYtW6vIUds8qR94A" target="_blank">
-            <img className='pagina-logo' src={youtube} alt='logo youtube'/>
-          </a>
+          <div className='caja'>
+            <a className="pagina" href="https://www.linkedin.com/in/amontoya224/" target="_blank">
+              <img className='pagina-logo' src={linkedin} alt='logo linkedin'/>
+            </a>
+          </div>
+          <div className='caja'>
+            <a className="pagina" href="https://github.com/AMontoya224" target="_blank">
+              <img className='pagina-logo' src={github} alt='logo github'/>
+            </a>
+          </div>
+          <div className='caja'>
+            <a className="pagina" href="https://scholar.google.com/citations?user=LI023coAAAAJ&hl=es" target="_blank">
+              <img className='pagina-logo' src={scholar} alt='logo scholar'/>
+            </a>
+          </div>
+          <div className='caja'>
+            <a className="pagina" href='https://drive.google.com/file/d/1VMg9fMJClXj3eSKCVz18-bn8OM4CR-tn/view?usp=sharing' target="_blank">
+              <img className='pagina-logo' src={cv} alt='logo cv'/>
+            </a>
+          </div>
+          <div className='caja'>
+            <a className="pagina" href="https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=140140" target="_blank">
+              <img className='pagina-logo' src={concytec} alt='logo concytec'/>
+            </a>
+          </div>
+          <div className='caja'>
+            <a className="pagina" href="https://web.facebook.com/juan.andres.montoya.angulo" target="_blank">
+              <img className='pagina-logo' src={facebook} alt='logo facebook'/>
+            </a>
+          </div>
+          <div className='caja'>
+            <a className="pagina" href="https://www.youtube.com/channel/UC94EeNbFYtW6vIUds8qR94A" target="_blank">
+              <img className='pagina-logo' src={youtube} alt='logo youtube'/>
+            </a>
+          </div>
         </div>
       </footer>
     </div>
