2aa1c63501c7d8ef4a54ca076b67e6924b44a992
Andres Montoya Angulo <jmontoyaan@unsa.edu.pe>
2022-04-30 10:04:22 -0500

    add cv

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import linkedin from './images/linkedin.png';
 import github from './images/github.png';
 import scholar from './images/scholar.png';
+import concytec from './images/concytec.png';
+import cv from './images/cv.jpg';
 import facebook from './images/facebook.png';
 import youtube from './images/youtube.png';
 import './App.css';
@@ -36,6 +38,12 @@ function App() {
           </a>
           <a className="pagina" href="https://scholar.google.com/citations?user=LI023coAAAAJ&hl=es" target="_blank">
             <img className='pagina-logo' src={scholar} alt='logo scholar'/>
+          </a>
+          <a className="pagina" src='./images/AMontoya_CV.pdf' target="_blank">
+            <img className='pagina-logo' src={cv} alt='logo cv'/>
+          </a>
+          <a className="pagina" href="https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=140140" target="_blank">
+            <img className='pagina-logo' src={concytec} alt='logo concytec'/>
           </a>
           <a className="pagina" href="https://web.facebook.com/juan.andres.montoya.angulo" target="_blank">
             <img className='pagina-logo' src={facebook} alt='logo facebook'/>
